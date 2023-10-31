dbe6a32a6e959139b494a34a3de5b00e528e8ff6
Hassan mirzaei <mirzaei@jumbula.com>
2023-10-31 18:03:30 +0330

    Delete additional entity information

--- TechsnovelTechnicalTask.Domain/AuditableEntity.cs
+++ TechsnovelTechnicalTask.Domain/AuditableEntity.cs
@@ -1,6 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-
 namespace TechsnovelTechnicalTask.Domain.Entities
 {
     public abstract class AuditableEntity<T> : Entity<T>, IAuditableEntity
@@ -8,34 +7,17 @@ namespace TechsnovelTechnicalTask.Domain.Entities
         [ScaffoldColumn(false)]
         [DisplayFormat(DataFormatString = "{0: HH:mm, yy/MM/dd}")]
         [Display(Name = "Create Date ")]
-
         public DateTime CreatedDate { get; set; }
-
-        [ScaffoldColumn(false)]
-        [Display(Name = "Create by ")]
-        [MaxLength(250)]
-        public string CreatedBy { get; set; }
 
         [ScaffoldColumn(false)]
         [DisplayFormat(DataFormatString = "{0: HH:mm, yy/MM/dd}")]
         [Display(Name = "Update Date")]
         public DateTime UpdatedDate { get; set; }
 
-        [Display(Name = "Update by")]
-        [ScaffoldColumn(false)]
-        [MaxLength(250)]
-        public string UpdatedBy { get; set; }
-
         [ScaffoldColumn(false)]
         [DisplayFormat(DataFormatString = "{0: HH:mm, yy/MM/dd}")]
         [Display(Name = "Delete Date")]
         public DateTime? DeletedDate { get; set; }
-
-        [Display(Name = "Delete by")]
-        [ScaffoldColumn(false)]
-        [MaxLength(250)]
-        public string? DeletedBy { get; set; }
-
 
     }
     public abstract class AuditableEntity : AuditableEntity<long>
