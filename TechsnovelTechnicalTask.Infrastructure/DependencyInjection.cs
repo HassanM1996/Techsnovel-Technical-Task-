1ec34f132f1f2a9b2dd4ee34885aaf017baaa6a4
Hassan mirzaei <mirzaei@jumbula.com>
2023-10-31 18:33:17 +0330

    Set Infrastructure Class library

---
+++ TechsnovelTechnicalTask.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using TechsnovelTechnicalTask.Application.Interfaces.FacadPatterns;
+using TechsnovelTechnicalTask.Infrastructure.FacadPattern;
+
+namespace TechsnovelTechnicalTask.Infrastructure
+{
+    public static class DependencyInjection
+
+    {
+        public static IServiceCollection AddInfrastructureDependency(this IServiceCollection services)
+        {
+            services.AddScoped<IProductFacad, ProductFacad>();
+            services.AddScoped<ICategoryFacad, CategoryFacad>();
+
+            return services;
+        }
+    }
+}
---
+++ TechsnovelTechnicalTask.Infrastructure/FacadPattern/CategoryFacad.cs
@@ -0,0 +1,41 @@
+﻿using TechsnovelTechnicalTask.Application.Interfaces.Contexts;
+using TechsnovelTechnicalTask.Application.Interfaces.FacadPatterns;
+using TechsnovelTechnicalTask.Application.Services.Commands.Category;
+using TechsnovelTechnicalTask.Application.Services.Queries.Category;
+
+namespace TechsnovelTechnicalTask.Infrastructure.FacadPattern
+{
+    public class CategoryFacad : ICategoryFacad
+    {
+        private readonly ISqlDataBaseContext _context;
+        public CategoryFacad(ISqlDataBaseContext context)
+        {
+            _context = context;
+        }
+
+        private IAddCategoryService _addCategoryService;
+        public IAddCategoryService AddCategoryService
+        {
+            get { return _addCategoryService = _addCategoryService ?? new AddCategoryService(_context); }
+        }
+        
+        private IUpdateCategoryService _updateCategoryService;
+        public IUpdateCategoryService UpdateCategoryService
+        {
+            get { return _updateCategoryService = _updateCategoryService ?? new UpdateCategoryService(_context); }
+        }
+        
+        private IGetCategoryService _getCategoryService;
+        public IGetCategoryService GetCategoryService
+        {
+            get { return _getCategoryService = _getCategoryService ?? new GetCategoryService(_context); }
+        }
+        
+        private IGetAllCategoryService _getAllCategoryService;
+        public IGetAllCategoryService GetAllCategoryService
+        {
+            get { return _getAllCategoryService = _getAllCategoryService ?? new GetAllCategoryService(_context); }
+        }
+
+    }
+}
